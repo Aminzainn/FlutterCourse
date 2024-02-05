59b945e2b52fa5cc7a4d7e2032599ce934706b7e
Aminzainn <zainnabil65@gmail.com>
2024-02-05 18:27:18 +0200

    last task

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,17 @@
-import 'package:course/welcome.dart';
 import 'package:flutter/material.dart';
-void main(){
-  runApp(SignInPage());
+import 'package:amin/home_page.dart';
 
-}
-class SignInPage extends StatefulWidget {
-  const SignInPage({super.key});
-
-  @override
-  State<SignInPage> createState() => _SignInPageState();
+void main() {
+  runApp(MyApp());
 }
 
-class _SignInPageState extends State<SignInPage> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar(centerTitle: true,title: Text("SIGN IN"),),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(children: [
-          FlutterLogo(size: 150),
-          //SizedBox(width: 50,height: 50,child:Image.asset("assets/logo.png",width:50,height:50)),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextFormField(keyboardType: TextInputType.phone,decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(),hintText: "phone"),validator: (value) {
-      if (value!.length>8&&value.length<10) {
-      return 'Please enter correct phone';
-      }
-      return null;
-      },),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextFormField(decoration: InputDecoration(
-                labelText: "password"),validator: (value) {
-          if (value!.length>8&&value.length<10) {
-          return 'Please enter correct password';
-          }
-          return null;
-          },),
-          ),
-          ElevatedButton(child: Text("login"), onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const welcomePage()),
-            );
-
-          }
-          ),
-
-        ],),
-      ),
-      ),
+      home: homePage(),
     );
   }
 }
