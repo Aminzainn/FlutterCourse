73d6b0a2326edbe88bb3e84267bf5e9a00907896
Aminzainn <zainnabil65@gmail.com>
2024-02-07 22:47:41 +0200

    final task

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,66 +1,132 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class homePage extends StatefulWidget {
-  const homePage({Key? key}) : super(key: key);
-
+class homePage extends StatelessWidget {
   @override
-  State<homePage> createState() => _homePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Authentication Example',
+      home: AuthenticationWrapper(),
+    );
+  }
 }
 
-class _homePageState extends State<homePage> {
-  List data = [];
-  bool loding = false;
+class AuthenticationWrapper extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: _auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else {
+          if (snapshot.hasData) {
+            return CategoryPage();
+          } else {
+            return LoginPage();
+          }
+        }
+      },
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "show data",
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w300),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white54,
-        ),
-        body: ListView(children: [
-          Container(
-              height: 50,
-              alignment: Alignment.topCenter,
-              margin: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  color: Colors.redAccent,
-                  borderRadius: BorderRadius.circular(15)),
-              child: InkWell(
-                child: Text(
-                  "Click to show",
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                  textAlign: TextAlign.center,
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
                 ),
-                onTap: () async {
-                  loding = true;
-                  setState(() {});
-                  var respo = await get(
-                      Uri.parse("https://jsonplaceholder.typicode.com/posts"));
-                  var respobody = jsonDecode(respo.body);
-                  data.addAll(respobody);
-                  loding = false;
-                  setState(() {});
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () async {
+                  String email = _emailController.text.trim();
+                  String password = _passwordController.text.trim();
+
+                  try {
+                    UserCredential userCredential =
+                        await _auth.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    User? user = userCredential.user;
+                    if (user != null) {
+                      // تسجيل الدخول بنجاح
+                      print('User signed in: ${user.uid}');
+                    }
+                  } catch (e) {
+                    // خطأ في تسجيل الدخول
+                    print('Failed to sign in: $e');
+                  }
                 },
-              )),
-          if (loding)
-            Center(
-              child: CircularProgressIndicator(),
-            ),
-          ...List.generate(
-              data.length,
-              (index) => Card(
-                    child: ListTile(
-                      title: Text("${data[index]['title']}"),
-                      subtitle: Text("${data[index]['body']}"),
-                    ),
-                  ))
-        ]));
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryPage extends StatelessWidget {
+  final FirebaseAuth _auth;
+
+  CategoryPage(this._auth);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () async {
+              await _auth.signOut();
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => LoginPage(),
+              ));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Category Page Content'),
+      ),
+    );
   }
 }
